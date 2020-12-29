565fcd1f2a22f775f9a3871eb9db58bd6f656147
Joseph <33774857+Shorty085@users.noreply.github.com>
2020-12-29 01:28:51 -0600

    Add files via upload

---
+++ Percent.py
@@ -0,0 +1,13 @@
+#THIS APP GIVES YOU THE GIVEN PERCENT OF ANY NUMBER
+#EXAMPLE 10% OF 100 IS 10
+
+percent = int(input("Enter the desired percentage amount: "))
+
+print("You have entered a percentage of {}%".format(percent))
+
+amount = int(input("What is the amount you want to calculate {}% of: ".format(percent)))
+
+calc1 = percent * amount
+calc2 = calc1 / 100
+
+print("{}% of ${} is {}\n".format(percent,amount,calc2))
